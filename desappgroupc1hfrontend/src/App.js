93905739891dbaf4af96bbec2ec7f77f6a38c639
Miloro <miloromiguel@gmail.com>
2019-09-02 17:58:38 -0300

    react y spring funcionando

--- desappgroupc1hfrontend/src/App.js
+++ desappgroupc1hfrontend/src/App.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    state = {};
+
+    componentDidMount() {
+        setInterval(this.hello, 250);
+    }
+
+    hello = () => {
+        fetch('/api/hello')
+            .then(response => response.text())
+            .then(message => {
+                this.setState({message: message});
+            });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">{this.state.message}</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
