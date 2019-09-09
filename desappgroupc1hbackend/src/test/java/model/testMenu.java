756faecca6d5c7dddbd721db44ac3d49944a2a77
Miloro <miloromiguel@gmail.com>
2019-09-09 14:00:41 -0300

    se agrega un test de prueba

---
+++ desappgroupc1hbackend/src/test/java/model/testMenu.java
@@ -0,0 +1,13 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class testMenu {
+    @Test
+    public void test() {
+        assertEquals(12,12);
+    }
+}
