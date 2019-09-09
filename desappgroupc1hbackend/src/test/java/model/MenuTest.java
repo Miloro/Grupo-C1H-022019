f9bcabec8d97e4834ab0cb9afa66fa0b840e3041
Miloro <miloromiguel@gmail.com>
2019-09-09 14:43:02 -0300

    se agrega un test de prueba para que se rompa travis2

--- desappgroupc1hbackend/src/test/java/model/MenuTest.java
+++ desappgroupc1hbackend/src/test/java/model/MenuTest.java
@@ -4,8 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-
-public class testMenu {
+public class MenuTest {
     @Test
     public void test1() {
         assertEquals(12,12);
