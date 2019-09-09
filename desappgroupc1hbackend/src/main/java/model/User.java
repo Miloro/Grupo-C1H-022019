0489c6c05285c8b4baba9da3d2f8b10a9898321b
Miloro <miloromiguel@gmail.com>
2019-09-09 17:54:52 -0300

    se crea el objeto usuario

---
+++ desappgroupc1hbackend/src/main/java/model/User.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.List;
+
+public class User {
+    String name;
+    String lastName;
+    String email;
+    Integer phoneNumber;
+    String city;
+    String adress;
+    Integer balance;
+    List<Menu> pendingMenusToScore;
+}
