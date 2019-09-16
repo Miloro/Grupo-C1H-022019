c4fdca551021640bbe6a03aa475abb2ca56a8d6c
lisar01 <lisar.3467@gmail.com>
2019-09-16 17:41:00 -0300

    agregando constr con vars y sin vars; getters y setters a todas las clases

--- desapp-groupc1h-backend/src/main/java/com/viandasya/model/Menu.java
+++ desapp-groupc1h-backend/src/main/java/com/viandasya/model/Menu.java
@@ -19,4 +19,146 @@ public class Menu {
     private Integer minAmountPrice2;
     private Integer maxAmountPerDay;
     private  List<Score> scores;
+
+    public Menu(String name, String description, List<Category> category, Integer deliveryPrice, LocalDate validSince, LocalDate validFrom, List<LocalDate> deliveryTime, Integer price, Integer averageDeliveryTime, Integer minAmount, Integer minAmount2, Integer minAmountPrice, Integer minAmountPrice2, Integer maxAmountPerDay, List<Score> scores) {
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.deliveryPrice = deliveryPrice;
+        this.validSince = validSince;
+        this.validFrom = validFrom;
+        this.deliveryTime = deliveryTime;
+        this.price = price;
+        this.averageDeliveryTime = averageDeliveryTime;
+        this.minAmount = minAmount;
+        this.minAmount2 = minAmount2;
+        this.minAmountPrice = minAmountPrice;
+        this.minAmountPrice2 = minAmountPrice2;
+        this.maxAmountPerDay = maxAmountPerDay;
+        this.scores = scores;
+    }
+
+    public Menu() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Category> getCategory() {
+        return category;
+    }
+
+    public void setCategory(List<Category> category) {
+        this.category = category;
+    }
+
+    public Integer getDeliveryPrice() {
+        return deliveryPrice;
+    }
+
+    public void setDeliveryPrice(Integer deliveryPrice) {
+        this.deliveryPrice = deliveryPrice;
+    }
+
+    public LocalDate getValidSince() {
+        return validSince;
+    }
+
+    public void setValidSince(LocalDate validSince) {
+        this.validSince = validSince;
+    }
+
+    public LocalDate getValidFrom() {
+        return validFrom;
+    }
+
+    public void setValidFrom(LocalDate validFrom) {
+        this.validFrom = validFrom;
+    }
+
+    public List<LocalDate> getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public void setDeliveryTime(List<LocalDate> deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getAverageDeliveryTime() {
+        return averageDeliveryTime;
+    }
+
+    public void setAverageDeliveryTime(Integer averageDeliveryTime) {
+        this.averageDeliveryTime = averageDeliveryTime;
+    }
+
+    public Integer getMinAmount() {
+        return minAmount;
+    }
+
+    public void setMinAmount(Integer minAmount) {
+        this.minAmount = minAmount;
+    }
+
+    public Integer getMinAmount2() {
+        return minAmount2;
+    }
+
+    public void setMinAmount2(Integer minAmount2) {
+        this.minAmount2 = minAmount2;
+    }
+
+    public Integer getMinAmountPrice() {
+        return minAmountPrice;
+    }
+
+    public void setMinAmountPrice(Integer minAmountPrice) {
+        this.minAmountPrice = minAmountPrice;
+    }
+
+    public Integer getMinAmountPrice2() {
+        return minAmountPrice2;
+    }
+
+    public void setMinAmountPrice2(Integer minAmountPrice2) {
+        this.minAmountPrice2 = minAmountPrice2;
+    }
+
+    public Integer getMaxAmountPerDay() {
+        return maxAmountPerDay;
+    }
+
+    public void setMaxAmountPerDay(Integer maxAmountPerDay) {
+        this.maxAmountPerDay = maxAmountPerDay;
+    }
+
+    public List<Score> getScores() {
+        return scores;
+    }
+
+    public void setScores(List<Score> scores) {
+        this.scores = scores;
+    }
+
 }
--- desapp-groupc1h-backend/src/main/java/com/viandasya/model/Service.java
+++ desapp-groupc1h-backend/src/main/java/com/viandasya/model/Service.java
@@ -1,6 +1,6 @@
 package com.viandasya.model;
 
-public class Service{
+public class Service {
     private String name;
     private String logo;
     private String adress;
@@ -9,4 +9,82 @@ public class Service{
     private String eMail;
     private Integer phoneNumber;
     private String serviceHours;
+
+    public Service(String name, String logo, String adress, String description, String website, String eMail, Integer phoneNumber, String serviceHours) {
+        this.name = name;
+        this.logo = logo;
+        this.adress = adress;
+        this.description = description;
+        this.website = website;
+        this.eMail = eMail;
+        this.phoneNumber = phoneNumber;
+        this.serviceHours = serviceHours;
+    }
+
+    public Service() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getServiceHours() {
+        return serviceHours;
+    }
+
+    public void setServiceHours(String serviceHours) {
+        this.serviceHours = serviceHours;
+    }
 }
