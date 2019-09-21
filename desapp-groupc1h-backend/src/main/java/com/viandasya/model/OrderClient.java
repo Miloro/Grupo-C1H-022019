5adac0f4f3e9219a8e006e9e21b4a76472c0c9ca
Miloro <miloromiguel@gmail.com>
2019-09-21 15:37:00 -0300

    se crea el builder del OrderClient y OrderMenu

--- desapp-groupc1h-backend/src/main/java/com/viandasya/model/OrderClient.java
+++ desapp-groupc1h-backend/src/main/java/com/viandasya/model/OrderClient.java
@@ -13,6 +13,52 @@ public class OrderClient extends AbstractOrder {
     public OrderClient() {
     }
 
+    public static class Builder{
+        private OrderState state;
+        private LocalDateTime orderDate;
+        private Integer score;
+        private Integer price;
+        private Integer amount;
+        private Menu menu;
+
+        public Builder(){}
+
+        public Builder setState(OrderState state){
+            this.state = state;
+            return this;
+        }
+
+        public Builder setOrderDate(LocalDateTime orderDate){
+            this.orderDate = orderDate;
+            return this;
+        }
+
+        public Builder setScore(Integer score){
+            this.score = score;
+            return this;
+        }
+
+        public Builder setPrice(Integer price){
+            this.price = price;
+            return this;
+        }
+
+        public Builder setAmount(Integer amount){
+            this.amount = amount;
+            return this;
+        }
+
+        public Builder setMenu(Menu menu){
+            this.menu = menu;
+            return this;
+        }
+
+        public OrderClient build(){
+            return new OrderClient(state, orderDate, score, price, amount, menu);
+        }
+
+    }
+
     public Menu getMenu() {
         return menu;
     }
--- desapp-groupc1h-backend/src/main/java/com/viandasya/model/OrderMenu.java
+++ desapp-groupc1h-backend/src/main/java/com/viandasya/model/OrderMenu.java
@@ -15,6 +15,59 @@ public class OrderMenu extends AbstractOrder {
     public OrderMenu() {
     }
 
+    public static class Builder{
+        private OrderState state;
+        private LocalDateTime orderDate;
+        private Integer score;
+        private Integer price;
+        private Integer amount;
+        private LocalDateTime deliveryDate;
+        private Boolean isDelivery;
+
+
+        public Builder(){}
+
+        public Builder setState(OrderState state){
+            this.state = state;
+            return this;
+        }
+
+        public Builder setOrderDate(LocalDateTime orderDate){
+            this.orderDate = orderDate;
+            return this;
+        }
+
+        public Builder setScore(Integer score){
+            this.score = score;
+            return this;
+        }
+
+        public Builder setPrice(Integer price){
+            this.price = price;
+            return this;
+        }
+
+        public Builder setAmount(Integer amount){
+            this.amount = amount;
+            return this;
+        }
+
+        public Builder setDeliveryDate(LocalDateTime deliveryDate){
+            this.deliveryDate = deliveryDate;
+            return this;
+        }
+
+        public Builder setIsDelivery(Boolean isDelivery){
+            this.isDelivery = isDelivery;
+            return this;
+        }
+
+        public OrderMenu build(){
+            return new OrderMenu(state, orderDate, score, price, amount, deliveryDate, isDelivery);
+        }
+
+    }
+
     public LocalDateTime getDeliveryDate() {
         return deliveryDate;
     }
