82a72b9de94173660959f585c2d065213040bb51
lisar01 <lisar.3467@gmail.com>
2019-11-19 23:57:28 -0300

    fix a navbar

--- desapp-groupc1h-frontend/src/components/NavBar.jsx
+++ desapp-groupc1h-frontend/src/components/NavBar.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import {Col, Menu, Row, Typography} from "antd";
+import {Button, Col, Menu, Row, Typography} from "antd";
 import MenuSearchInput from "../routes/menus/MenuSearchInput";
+import {useAuth0} from "../providers/Auth0Provider";
+import {useUser} from "../providers/UserProvider";
+import {FormattedMessage} from "react-intl";
+
 const {Title} = Typography;
 
 const menuProps = {
@@ -9,33 +13,40 @@ const menuProps = {
     style: {lineHeight: '62px'},
 };
 
-const logoProps= {
-    style:{
+const logoProps = {
+    style: {
         fontFamily: "Arial, Helvetica, sans-serif",
         marginTop: 5,
         fontColor: "#FF8C00"
     }
 };
 
-function NavBar(props) {
-    return (
-        <Row>
-            <Col span={5}>
-                <div {...logoProps}>
-                    <Title> Viandas Ya</Title>
-                </div>
-            </Col>
-            <Col span={12} style={{ paddingTop: '1%' }}>
-                <MenuSearchInput/>
-            </Col>
-            <Col span={7}>
-                <Menu {...menuProps}>
-                    <Menu.Item key="1">nav 1</Menu.Item>
-                    <Menu.Item key="2">nav 2</Menu.Item>
-                </Menu>
-            </Col>
-        </Row>
-    );
-}
+const NavBar = () => {
+    const {isAuthenticated, loading, logout} = useAuth0();
+    const [{id}, ] = useUser();
+
+    return <Row>
+        <Col span={5}>
+            <div {...logoProps}>
+                <Title> Viandas Ya</Title>
+            </div>
+        </Col>
+        <Col span={12} style={{paddingTop: '1%'}}>
+            <MenuSearchInput/>
+        </Col>
+        <Col span={7}>
+            <Menu {...menuProps}>
+                <Menu.Item key="1">nav 1</Menu.Item>
+                <Menu.Item key="2">nav 2</Menu.Item>
+                <Menu.Item key="3">
+                    {(!loading) && isAuthenticated && id &&
+                    <Button type="primary" onClick={() => logout()}>
+                        <FormattedMessage id="logout"/>
+                    </Button>}
+                </Menu.Item>
+            </Menu>
+        </Col>
+    </Row>
+};
 
 export default NavBar;
